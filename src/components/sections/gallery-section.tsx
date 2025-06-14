61f0924442c617c3d4838305392c554c1b4632e8
Rae-An Andres <raeandres@users.noreply.github.com>
2025-06-15 04:36:31 +0800

    Updated assets

--- src/components/sections/gallery-section.tsx
+++ src/components/sections/gallery-section.tsx
@@ -33,6 +33,7 @@ const galleryItems: GalleryCategory[] = [
       hint: "Minimalist 1 bedroom suite",
     },
     images: [
+      // Living Room
       {
         src: "/kirei_2/gallery/livingroom/living_sofa_cp.webp",
         alt: "Living Room - Kirei 2",
@@ -57,6 +58,131 @@ const galleryItems: GalleryCategory[] = [
         src: "/kirei_2/gallery/livingroom/living_kitchen_1.webp",
         alt: "Living Room - View 3",
         hint: "Kirei 2 - living 4",
+      },
+      // Bedroom
+      {
+        src: "/kirei_2/gallery/bedroom/bed_window_angle_2_cp.webp",
+        alt: "Bed Room - View 1",
+        hint: "Kirei 2 - Bed 1",
+      },
+      {
+        src: "/kirei_2/gallery/bedroom/bed_window_cp.webp",
+        alt: "Bed Room - View 2",
+        hint: "Kirei 2 - Bed 2",
+      },
+      {
+        src: "/kirei_2/gallery/bedroom/bed_window_left_cp.webp",
+        alt: "Bed Room - View 3",
+        hint: "Kirei 2 - Bed 3",
+      },
+      {
+        src: "/kirei_2/gallery/bedroom/bed_window_right_cp.webp",
+        alt: "Bed Room - View 4",
+        hint: "Kirei 2 - Bed 4",
+      },
+      {
+        src: "/kirei_2/gallery/bedroom/bedroom_front_1.webp",
+        alt: "Bed Room - View 5",
+        hint: "Kirei 2 - Bed 5",
+      },
+      {
+        src: "/kirei_2/gallery/bedroom/bedroom_front_2.webp",
+        alt: "Bed Room - View 6",
+        hint: "Kirei 2 - Bed 6",
+      },
+      {
+        src: "/kirei_2/gallery/bedroom/bedroom_right_angle_1.webp",
+        alt: "Bed Room - View 7",
+        hint: "Kirei 2 - Bed 7",
+      },
+      // Kitchen
+      {
+        src: "/kirei_2/gallery/kitchen/dining_angle_1.webp",
+        alt: "Kitchen  - View 1",
+        hint: "Kirei 2 - Kitchen 1",
+      },
+      {
+        src: "/kirei_2/gallery/kitchen/dining_plates_chair_cp.webp",
+        alt: "Kitchen  - View 2",
+        hint: "Kirei 2 - Kitchen 2",
+      },
+      {
+        src: "/kirei_2/gallery/kitchen/dining_plates_cp.webp",
+        alt: "Kitchen  - View 3",
+        hint: "Kirei 2 - Kitchen 3",
+      },
+      {
+        src: "/kirei_2/gallery/kitchen/kitchen_1.webp",
+        alt: "Kitchen  - View 4",
+        hint: "Kirei 2 - Kitchen 4",
+      },
+      {
+        src: "/kirei_2/gallery/kitchen/kitchen_dining_cp.webp",
+        alt: "Kitchen  - View 5",
+        hint: "Kirei 2 - Kitchen 5",
+      },
+      {
+        src: "/kirei_2/gallery/kitchen/kitchen_plates.webp",
+        alt: "Kitchen  - View 6",
+        hint: "Kirei 2 - Kitchen 6",
+      },
+      {
+        src: "/kirei_2/gallery/kitchen/oven_angle.webp",
+        alt: "Kitchen  - View 7",
+        hint: "Kirei 2 - Kitchen 7",
+      },
+      {
+        src: "/kirei_2/gallery/kitchen/rice_cooker.webp",
+        alt: "Kitchen  - View 8",
+        hint: "Kirei 2 - Kitchen 8",
+      },
+      {
+        src: "/kirei_2/gallery/kitchen/stove_angle.webp",
+        alt: "Kitchen  - View 9",
+        hint: "Kirei 2 - Kitchen 9",
+      },
+      // Bathroom
+      {
+        src: "/kirei_2/gallery/bathroom/toilet_and_bath_cp.webp",
+        alt: "Bath  - View 1",
+        hint: "Kirei 2 - Laundry 1",
+      },
+      // Laundry
+      {
+        src: "/kirei_2/gallery/laundry/laundry_1.webp",
+        alt: "Laundry  - View 1",
+        hint: "Kirei 2 - Laundry 1",
+      },
+      {
+        src: "/kirei_2/gallery/laundry/laundry_2jpg.webp",
+        alt: "Laundry  - View 2",
+        hint: "Kirei 2 - Laundry 2",
+      },
+      {
+        src: "/kirei_2/gallery/laundry/laundry_angle_cp.webp",
+        alt: "Laundry  - View 3",
+        hint: "Kirei 2 - Laundry 3",
+      },
+      {
+        src: "/kirei_2/gallery/laundry/laundry_front.webp",
+        alt: "Laundry  - View 4",
+        hint: "Kirei 2 - Laundry 4",
+      },
+      // Amenities
+      {
+        src: "/kirei_2/amenities/converted_0005.webp",
+        alt: "Amenities  - View 1",
+        hint: "Kirei 2 - Amenities 1",
+      },
+      {
+        src: "/kirei_2/amenities/converted_0003.webp",
+        alt: "Amenities  - View 2",
+        hint: "Kirei 2 - Amenities 2",
+      },
+      {
+        src: "/kirei_2/amenities/converted_0002.webp",
+        alt: "Amenities  - View 3",
+        hint: "Kirei 2 - Amenities 3",
       },
     ],
   },
