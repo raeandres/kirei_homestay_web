d0a93843ce06c54863446078c6c2c62f71dc7178
Rae-An Andres <raeandres@users.noreply.github.com>
2025-06-27 02:03:01 +0800

    Updated font and paragraph aesthetics

--- src/components/sections/about-section.tsx
+++ src/components/sections/about-section.tsx
@@ -18,17 +18,25 @@ export function AboutSection() {
             Your browser does not support the video tag.
           </video>
         </div>
-        <p className="text-lg md:text-xl text-foreground/80 mb-6 leading-relaxed px-4 sm:px-0">
-          kirei (綺麗), meaning beautiful and clean in Japanese, is more than
-          just a place to stay. It's an invitation to embrace slow, intentional
-          living. Our space is thoughtfully designed to be a sanctuary of calm,
-          where minimalist aesthetics meet cozy comfort.
-        </p>
-        <p className="text-lg md:text-xl text-foreground/80 leading-relaxed px-4 sm:px-0">
-          We believe in the beauty of simplicity and the importance of mindful
-          moments. Every detail at Kirei House is curated to help you unwind,
-          reconnect, and find joy in the present.
-        </p>
+        <div className="px-10 font-zen-old-mincho">
+          <p className="text-lg text-right md:text-xl text-foreground/80 mb-2 leading-relaxed px-4 sm:px-0">
+            kirei (綺麗), meaning beautiful and clean in Japanese, is more than
+            just a place to stay.
+          </p>
+          <p className="text-lg text-right md:text-xl text-foreground/80 mb-2 leading-relaxed px-4 sm:px-0">
+            It's an invitation to embrace slow, intentional living. Our space is
+            thoughtfully designed to be a sanctuary of calm, where minimalist
+            aesthetics meet cozy comfort.
+          </p>
+          <p className="text-lg text-right md:text-xl text-foreground/80 mb-2 leading-relaxed px-4 sm:px-0">
+            We believe in the beauty of simplicity and the importance of mindful
+            moments.
+          </p>
+          <p className="text-lg text-right md:text-xl text-foreground/80 mb-2 leading-relaxed px-4 sm:px-0">
+            Every detail at Kirei House is curated to help you unwind,
+            reconnect, and find joy in the present.
+          </p>
+        </div>
       </div>
     </section>
   );
