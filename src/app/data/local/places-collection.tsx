6b711a3b1a91e7d3a1c9eb1fc272f8cf110d3c1c
Rae-An Andres <raeandres@users.noreply.github.com>
2025-09-07 15:55:40 +0800

    Code cleanup
    Moved data arrays to data/local directory

---
+++ src/app/data/local/places-collection.tsx
@@ -0,0 +1,11 @@
+const nearbyPlaces = [
+  { name: "Eastwood City", distance: "0.1 km" },
+  { name: "Bonifacio Global City", distance: "6 km" },
+  { name: "Ortigas Center", distance: "3.8 km" },
+  { name: "Makati", distance: "7 km" },
+  { name: "Airport", distance: "12 km" },
+];
+
+export const PlacesCollection = {
+    nearbyPlaces
+}
---
+++ src/app/data/local/reviews-collection.tsx
@@ -0,0 +1,87 @@
+const reviews = [
+  // {
+  //   name: "Hazel",
+  //   avatar: "https://placehold.co/100x102.png?text=H", // unique URL
+  //   avatarHint: "person avatar",
+  //   rating: 5,
+  //   review:
+  //     "I've had the pleasure of staying in numerous Airbnbs across countries, but Siri’s been one of the most helpful, warm, and accommodating host I've ever encountered. Our experience with Siri was exceptional from the get go.We had to evacuate during typhoon Carina, and we were fortunate to find her listing available. Despite our last-minute request, she was quick to confirm our stay, making the stressful situation much more manageableThe check-in process was seamless, and our stay was nothing short of amazing. Truly, our home away from home! Our 1-year-old mini poodle, Ragnar, surely enjoyed the plushies provided. Despite bringing his own toys, he quickly found new favorites among the ones Siri had prepared.Siri's hospitality made our stay memorable. If only we could have stayed longer! We highly recommend her place – a thousand percent. 🩶",
+  //   date: "July 2024",
+  // },
+  {
+    name: "Nicole",
+    avatar: "https://placehold.co/100x102.png?text=N", // unique URL
+    avatarHint: "person avatar",
+    rating: 5,
+    review:
+      "The place was immaculate! Just like in the photos. Was my most comfortable stay. I realized I left my Anker charger in the bnb the day after I checked out and it was delivered as soon as the next guest had checked out. Very thankful for that! I will most definitely be back here!",
+    date: "February 2025",
+  },
+  {
+    name: "Sanaya Michelle",
+    avatar: "https://placehold.co/100x102.png?text=SM", // unique URL
+    avatarHint: "person avatar",
+    rating: 5,
+    review:
+      "This is my go-to place whenever I'm in Quezon City. It's always clean, smells great, and is even more beautiful in person. The location in Eastwood is perfect—great restaurants and everything I need are just around the corner! Siri is incredibly friendly and always goes out of her way to make sure I'm well taken care of. Highly recommended!",
+    date: "March 2025",
+  },
+  // {
+  //   name: "Karen",
+  //   avatar: "https://placehold.co/100x102.png?text=K", // unique URL
+  //   avatarHint: "person avatar",
+  //   rating: 5,
+  //   review:
+  //     "Had a perfect weekend at Kirei House — a relaxing haven right in the heart of the city. I loved the aesthetic and thoughtful design. Check-in was seamless, and the place had everything we needed — from toothbrushes and slippers to a hairdryer and even a steam iron.Siri and Toti are fantastic hosts — super responsive and know the good restaurants and cafes nearby if you're not sure where to go. Staying in was hard to resist tho, so we decided to cook on our 2nd day instead of eating out. To our surprise, the kitchen had everything we needed too. We didn’t get a chance to use the building amenities, but the pool looked great, and there was a parking space available as well.I guess the only downside is that I don’t actually live here lol. Will definitely book again and recommend this place to family and friends.Thank you, Kirei House!",
+  //   date: "May 2025",
+  // },
+  {
+    name: "Jannice",
+    avatar: "https://placehold.co/100x102.png?text=J", // unique URL
+    avatarHint: "person avatar",
+    rating: 5,
+    review:
+      "Beautiful home with stunning view. It has everything we need and more. We will definitely come back. Thank you Siri ❤️",
+    date: "May 2025",
+  },
+  {
+    name: "Angel",
+    avatar: "https://placehold.co/100x100.png?text=A",
+    avatarHint: "person avatar",
+    rating: 5,
+    review:
+      "We held my bridal shower here and had a wonderful time! The place was clean, and the interior had such a relaxing vibe. It had everything we needed from utensils and toiletries to laundry essentials. If we ever have errands in QC again, we’d definitely book this place. As a bonus, the host was very responsive and accommodating to our requests!",
+    date: "May 2025",
+  },
+  {
+    name: "Miguel",
+    avatar: "https://placehold.co/100x101.png?text=M", // unique URL
+    avatarHint: "person avatar",
+    rating: 5,
+    review:
+      "Siri’s place is hands-down the nicest Airbnb I’ve stayed in. It’s spotless, tastefully designed, and stocked with everything you’d need. Siri was super responsive and made the whole stay effortless.",
+    date: "June 2025",
+  },
+  {
+    name: "Marie",
+    avatar: "https://placehold.co/100x102.png?text=M", // unique URL
+    avatarHint: "person avatar",
+    rating: 5,
+    review:
+      "Really enjoyed our stay here! The host was very responsive and helpful, and the place was super clean. The location was extremely convenient—just a short trip to all our appointments. Would totally book again!",
+    date: "June 2025",
+  },
+  {
+    name: "Marione Yolene",
+    avatar: "https://placehold.co/100x102.png?text=MY", // unique URL
+    avatarHint: "person avatar",
+    rating: 5,
+    review:
+      "The place really deserves the title of Muji House with its minimalistic yet elegant design with the right touch of Japanese elements. It really felt like home that even my 2 pets, Moonshine and Sunbeam, felt at home the moment we got there. Also, there are many great restaurants and convenience stores around the area which made our stay hassle-free. Perfect for a staycation with loved ones (furbabies included) 💖",
+    date: "June 2025",
+  },
+];
+
+export const ReviewsCollection = {
+    reviews
+}
